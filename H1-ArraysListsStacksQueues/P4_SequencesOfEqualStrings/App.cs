7b742ad0bee7f1500fad7714c6c6c073f4703bb6
Svetlozar Kirkov <svetlozark@gmail.com>
2015-10-09 16:26:37 +0300

    finished h1-p4

---
+++ H1-ArraysListsStacksQueues/P4_SequencesOfEqualStrings/App.cs
@@ -0,0 +1,35 @@
+﻿namespace P4_SequencesOfEqualStrings
+{
+    using System;
+
+    internal sealed class App
+    {
+        internal static void Main()
+        {
+            Console.Write("Enter strings (separated by space): ");
+            var inputArray = Console.ReadLine().Split(' ');
+            
+            for (int currentIndex = 0; currentIndex < inputArray.Length;)
+            {
+                Console.Write(inputArray[currentIndex]);
+                var currentString = inputArray[currentIndex];
+
+                for (int nextIndex = currentIndex + 1; nextIndex < inputArray.Length; nextIndex++)
+                {
+                    if (inputArray[nextIndex].Equals(currentString))
+                    {
+                        Console.Write(" {0}", inputArray[nextIndex]);
+                        currentIndex++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                Console.WriteLine();
+                currentIndex++;
+            }
+        }
+    }
+}
