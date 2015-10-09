a341001733e4ed76bc63a10c7de6631a0e80ffe5
Svetlozar Kirkov <svetlozark@gmail.com>
2015-10-09 16:04:52 +0300

    upd h1-p3

--- H1-ArraysListsStacksQueues/P3_CategorizeNumbersFindMinMaxAvg/App.cs
+++ H1-ArraysListsStacksQueues/P3_CategorizeNumbersFindMinMaxAvg/App.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-
-namespace P3_CategorizeNumbersFindMinMaxAvg
+﻿namespace P3_CategorizeNumbersFindMinMaxAvg
 {
     using System;
     using System.Linq;
@@ -12,37 +10,29 @@ namespace P3_CategorizeNumbersFindMinMaxAvg
             Console.Write("Enter floating-point numbers (separated by space): ");
             var inputLine = Console.ReadLine();
             if (!string.IsNullOrEmpty(inputLine))
-            {
-                List<double> hasFractionNums = new List<double>();
-                List<double> noFractionNums = new List<double>();
-                
-                double[] numbers = inputLine
+            {           
+                double[] inputNumbers = inputLine
                     .Split(' ')
                     .Select(double.Parse)
                     .ToArray();
-
-                foreach (var num in numbers)
-                {
-                    if (num % 1 == 0)
-                    {
-                        noFractionNums.Add(num);
-                    }
-                    else
-                    {
-                        hasFractionNums.Add(num);
-                    }
-                }
                 
-                PrintNumbersList("Numbers with fraction:", hasFractionNums);
-                PrintNumbersList("Numbers with zero fraction:", noFractionNums);
+                double[] numbersWithFraction = inputNumbers
+                    .Where(x => x % 1 != 0)
+                    .ToArray();
+                
+                double[] numbersWithoutFraction = inputNumbers
+                    .Where(x => x % 1 == 0)
+                    .ToArray();
+                
+                PrintResult(numbersWithFraction);
+                PrintResult(numbersWithoutFraction);
             }
         }
 
-        internal static void PrintNumbersList(string introAppend, List<double> numbers)
+        internal static void PrintResult(double[] numbers)
         {
             Console.WriteLine(
-                "{0} [ {1} ] -> min: {2}, max: {3}, sum: {4}, avg: {5:F2}",
-                introAppend,
+                "[ {0} ] -> min: {1}, max: {2}, sum: {3}, avg: {4:F2}",
                 string.Join(", ", numbers),
                 numbers.Min(),
                 numbers.Max(),
