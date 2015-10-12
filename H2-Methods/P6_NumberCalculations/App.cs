581ee84cc24a5a300f972965ca85bd5bf0e8eea6
Svetlozar Kirkov <svetlozark@gmail.com>
2015-10-12 20:38:59 +0300

    small upd

--- H2-Methods/P6_NumberCalculations/App.cs
+++ H2-Methods/P6_NumberCalculations/App.cs
@@ -1,18 +1,25 @@
 ﻿namespace P6_NumberCalculations
 {
-    using System.Collections.Generic;
+    using System;
 
     internal sealed class App
     {
         internal static void Main()
         {
-            var doubleSet = new List<double>
+            double[] doubleSet =
             {
-                1.5, 32.12, -4.24, 242.5353
+                1.5, 
+                32.12, 
+                -4.24, 
+                242.5353
             };
-            var decimalSet = new List<decimal>
+            
+            decimal[] decimalSet =
             {
-                1.524135464m, 32.142142m, -4.4241224m, -242.2142415353m
+                1.524135464m, 
+                32.142142m, 
+                -4.4241224m, 
+                -242.2142415353m
             };
         }
     }
