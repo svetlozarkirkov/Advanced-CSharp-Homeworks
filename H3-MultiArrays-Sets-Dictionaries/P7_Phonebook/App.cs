1296ee8cee5a7e24f5bbd9c85227ab99f6e83e4d
Svetlozar Kirkov <svetlozark@gmail.com>
2015-10-17 18:14:11 +0300

    finished h3-p7

--- H3-MultiArrays-Sets-Dictionaries/P7_Phonebook/App.cs
+++ H3-MultiArrays-Sets-Dictionaries/P7_Phonebook/App.cs
@@ -1,10 +1,52 @@
 ﻿namespace P7_Phonebook
 {
+    using System;
+    using System.Collections.Generic;
+
     internal sealed class App
     {
         internal static void Main()
         {
+            var phonebook = new Dictionary<string, List<string>>();
+            Console.WriteLine("Enter data in format NAME-PHONENUMBER:");
+            string currentLine = Console.ReadLine();
 
+            while (!currentLine.Equals("search"))
+            {
+                string[] splitData = currentLine.Split('-');
+                string name = splitData[0];
+                string phoneNumber = splitData[1];
+                
+                if (phonebook.ContainsKey(name))
+                {
+                    phonebook[name].Add(phoneNumber);
+                }
+                else
+                {
+                    phonebook.Add(name, new List<string> { phoneNumber });
+                }
+
+                currentLine = Console.ReadLine();
+            }
+
+            string searchedName = Console.ReadLine();
+            
+            while (!searchedName.Equals("END"))
+            {
+                if (phonebook.ContainsKey(searchedName))
+                {
+                    Console.WriteLine(
+                        "{0} -> [ {1} ]", 
+                        searchedName,
+                        string.Join(", ", phonebook[searchedName]));
+                }
+                else
+                {
+                    Console.WriteLine("Contact {0} does not exist.", searchedName);
+                }
+
+                searchedName = Console.ReadLine();
+            }
         }
     }
 }
