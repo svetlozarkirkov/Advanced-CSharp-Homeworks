3b3568467e2823178a72313af0a5532d5221f2b0
Svetlozar Kirkov <svetlozark@gmail.com>
2015-10-18 13:03:14 +0300

    finished h4-p5-p6

---
+++ H4-StringsAndTextProcessing/P5_UnicodeCharacters/App.cs
@@ -0,0 +1,20 @@
+﻿namespace P5_UnicodeCharacters
+{
+    using System;
+
+    internal sealed class App
+    {
+        internal static void Main()
+        {
+            Console.Write("String: ");
+            char[] stringChars = Console.ReadLine().ToCharArray();
+            Console.Write("Result: ");
+            foreach (char symbol in stringChars)
+            {
+                Console.Write("\\U{0:x4}", (int)symbol);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ H4-StringsAndTextProcessing/P6_Palindromes/App.cs
@@ -0,0 +1,49 @@
+﻿namespace P6_Palindromes
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal sealed class App
+    {
+        internal static void Main()
+        {
+            char[] delimiters = { ' ', ',', '.', '?', '!' };
+            var palindromes = new SortedSet<string>();
+            Console.Write("Text: ");
+            string[] words = Console.ReadLine().Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var word in words)
+            {
+                if (IsPalindrome(word) && !palindromes.Contains(word))
+                {
+                    palindromes.Add(word);
+                }
+            }
+
+            Console.WriteLine("Palindromes: {0}", string.Join(", ", palindromes));
+        }
+
+        private static bool IsPalindrome(string word)
+        {
+            int left = 0;
+            int right = word.Length - 1;
+            while (true)
+            {
+                if (left > right)
+                {
+                    return true;
+                }
+                
+                char a = word[left];
+                char b = word[right];
+                
+                if (a != b)
+                {
+                    return false;
+                }
+
+                left++;
+                right--;
+            }
+        }
+    }
+}
