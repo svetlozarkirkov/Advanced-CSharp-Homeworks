e91170d937d3cff5d9ddbe6ae6c2922ba80d2a31
Svetlozar Kirkov <svetlozark@gmail.com>
2015-10-18 12:32:46 +0300

    finished h4-p4

---
+++ H4-StringsAndTextProcessing/P4_TextFilter/App.cs
@@ -0,0 +1,34 @@
+﻿namespace P4_TextFilter
+{
+    using System;
+
+    internal sealed class App
+    {
+        internal static void Main()
+        {
+            Console.Write("Banned words:\n");
+            string[] bannedWords = Console.ReadLine().Split(
+                new[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
+            Console.Write("Text:\n");
+            char[] textCharacters = Console.ReadLine().ToCharArray();
+            
+            foreach (string bannedWord in bannedWords)
+            {
+                int bannedLength = bannedWord.Length;
+                
+                for (int textIndex = 0; textIndex < textCharacters.Length - bannedLength; textIndex++)
+                {
+                    if (new string(textCharacters, textIndex, bannedLength).Equals(bannedWord))
+                    {
+                        for (int index = textIndex; index < textIndex + bannedLength; index++)
+                        {
+                            textCharacters[index] = '*';
+                        }
+                    }
+                }
+            }
+            
+            Console.WriteLine("Result:\n{0}", new string(textCharacters));
+        }
+    }
+}
