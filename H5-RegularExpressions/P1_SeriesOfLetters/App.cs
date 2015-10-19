d3efc6d52029ca534067ee2ea2166adf09b47675
Svetlozar Kirkov <svetlozark@gmail.com>
2015-10-19 14:39:20 +0300

    initial h5 up

---
+++ H5-RegularExpressions/P1_SeriesOfLetters/App.cs
@@ -0,0 +1,23 @@
+﻿namespace P1_SeriesOfLetters
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    internal sealed class App
+    {
+        internal static void Main()
+        {
+            Console.Write("Enter string: ");
+            string inputLine = Console.ReadLine();
+            string pattern = @"([a-zA-Z])\1*";
+            MatchCollection matches = Regex.Matches(inputLine, pattern);
+            Console.Write("Result: ");
+            foreach (Match match in matches)
+            {
+                Console.Write(match.Groups[1].Value);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ H5-RegularExpressions/P2_ReplaceAnchorTag/App.cs
@@ -0,0 +1,26 @@
+﻿namespace P2_ReplaceAnchorTag
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    internal sealed class App
+    {
+        internal static void Main()
+        {
+            string firstString = @"<ul><li><a href=""http://softuni.bg"">SoftUni</a></li></ul>";
+            string secondString = @"<ul><li><a href='http://softuni.bg'>SoftUni</a></li></ul>";
+            Console.WriteLine(ReplaceAnchorTag(firstString));
+            Console.WriteLine(ReplaceAnchorTag(secondString));
+        }
+
+        private static string ReplaceAnchorTag(string input)
+        {
+            string pattern = @"(<a href=[""|'])([^>]+)([""|']>)(\S+)(<\/a>)";
+
+            return Regex.Replace(
+                input,
+                pattern,
+                m => "[URL href=" + m.Groups[2].Value + "]" + m.Groups[4].Value + "[/URL]");
+        }
+    }
+}
