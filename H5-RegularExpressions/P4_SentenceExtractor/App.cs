ee4918c4a7a8936ef8c759deb40daa37c0453461
Svetlozar Kirkov <svetlozark@gmail.com>
2015-10-20 13:57:46 +0300

    finished h5-p4

---
+++ H5-RegularExpressions/P4_SentenceExtractor/App.cs
@@ -0,0 +1,29 @@
+﻿namespace P4_SentenceExtractor
+{
+    using System;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    internal sealed class App
+    {
+        private const string SentencePattern = "([^.|^!|^?]+[?|!?.])";
+
+        internal static void Main()
+        {
+            Console.Write("Keyword: ");
+            string keyword = Console.ReadLine();
+            Console.Write("Text: ");
+            string text = Console.ReadLine();
+            
+            foreach (Match match in Regex.Matches(text, SentencePattern))
+            {
+                string currentSentence = match.Groups[0].Value.Trim();
+                string[] currentSentenceWords = currentSentence.Split(' ');
+                if (currentSentenceWords.Contains(keyword))
+                {
+                    Console.WriteLine(currentSentence);
+                }
+            }
+        }
+    }
+}
