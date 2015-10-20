b58245ad508b84877eceb0e0b660de1d2083bd06
Svetlozar Kirkov <svetlozark@gmail.com>
2015-10-20 17:31:18 +0300

    h6 update

---
+++ H6-Streams-Files/P4_CopyBinaryFile/App.cs
@@ -0,0 +1,18 @@
+﻿namespace P4_CopyBinaryFile
+{
+    using System.IO;
+
+    internal sealed class App
+    {
+        internal static void Main()
+        {
+            // The created file (kitten-copy.jpg) is in bin\Debug
+            // The usings are chained for less nesting
+            using (var copyStream = new FileStream("kitten-copy.jpg", FileMode.Create, FileAccess.Write))
+            using (var readStream = new FileStream("kitten.jpg", FileMode.Open, FileAccess.Read))
+            {
+                readStream.CopyTo(copyStream);
+            }
+        }
+    }
+}
---
+++ H6-Streams-Files/P5_SlicingFile/App.cs
@@ -0,0 +1,24 @@
+﻿namespace P5_SlicingFile
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    internal sealed class App
+    {
+        internal static void Main()
+        {
+            Slice("small.mp4", "C:\\OUTPUT", 3);
+        }
+
+        private static void Slice(string sourceFile, string destinationDirectory, int parts)
+        {
+            
+        }
+
+        private static void Assemble(List<string> files, string destinationDirectory)
+        {
+            
+        }
+    }
+}
